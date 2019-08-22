12ba320bd0ceea8b28f4a461c4c3f34b73b23171
Marcelo Matos <marcelo_matos@hotmail.com>
2019-08-22 01:10:53 +0000

    jQuery

---
+++ main.js
@@ -0,0 +1,16 @@
+function chamarAPI(){
+	console.log("Chamando API");
+	const endpoint = "https://api.exchangeratesapi.io/latest";
+	$.get(endpoint, callback );
+}
+
+function callback(resultado){
+	// console.log(resultado);
+	
+	var sMoedaDestino = $("#moeda_destino").val();
+	var fValor = resultado.rates[sMoedaDestino];
+	
+	$("#valor_destino").text(fValor);
+	$("#moeda_destino_celula").text(sMoedaDestino);
+}
+
