6c39a50537668a96fec4f8aadbc666d7426dcb6d
Marcelo Matos <marcelo_matos@hotmail.com>
2019-08-24 01:25:18 +0000

    refatoração: remoção de funções globais

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-function chamarAPI(){
-	console.log("Chamando API");
-	const endpoint = "api/latest";
-	var oParametros = {
-		base: $("#moeda_origem").val()
-	};
-	$.get(endpoint, oParametros, callback );
+const ovly = { };
+
+ovly.conversor = { 
+	versao: "1",
+	chamarAPI: function(){
+		console.log("Chamando API");
+		const endpoint = "api/latest";
+		var oParametros = {
+			base: $("#moeda_origem").val()
+		};
+		$.get(endpoint, oParametros, this.callback );
+	},
+	callback: function(resultado){
+		// console.log(resultado);
+		var sMoedaDestino = $("#moeda_destino").val();
+		var fValor = resultado.rates[sMoedaDestino];
+		$("#valor_destino").text(fValor);
+		$("#moeda_destino_celula").text(sMoedaDestino);
+	}
 }
 
-function callback(resultado){
-	// console.log(resultado);
-	
-	var sMoedaDestino = $("#moeda_destino").val();
-	var fValor = resultado.rates[sMoedaDestino];
-	
-	$("#valor_destino").text(fValor);
-	$("#moeda_destino_celula").text(sMoedaDestino);
-}
 
